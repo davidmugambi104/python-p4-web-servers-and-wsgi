066ed7856aaa309fdaa0e7aac6328e3dfa44adfc
davidmugambi104 <davidmugambi104@gmail.com>
2024-06-26 21:50:52 +0300

    werkzeun_server

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,15 @@
-#!/usr/bin/env python3
+# !/usr/bin/env  python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('i love you sana my love sharoh')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5700,
+        application=application
+    )
